58dd4d9c490010229b3ef8911bd628fda2663d85
Raynos <raynos2@gmail.com>
2015-08-17 16:19:19 +0200

    add script to download perl scripts

---
+++ scripts/import_flamegraphs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget -O ./bg-flamegraphs/stackcollapse-stap.pl \
+    https://raw.githubusercontent.com/brendangregg/FlameGraph/master/stackcollapse-stap.pl
+
+wget -O ./bg-flamegraphs/flamegraph.pl \
+    https://raw.githubusercontent.com/brendangregg/FlameGraph/master/flamegraph.pl
